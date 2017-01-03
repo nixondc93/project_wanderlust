fc5ae6f12a11299a08f346a992d17a60c54190e9
margarh1 <ha.margaret1@gmail.com>
2017-01-03 10:52:34 -0800

    wrote routes, created site, users, sessions, cities controllers

---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,9 @@
+class CitiesController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'site#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/profile' => 'users#show'
+  get '/edit' => 'users#edit'
+  put '/profile' => 'users#update'
+  patch '/profile' => 'users#update'
+
+  get '/cities' => 'cities#index'
+  get '/cities/:name' => 'cities#show', as: 'city'
+
 end
