d927763b6aec27c1872924e2d9850b321fc6336a
Dan Finelli <dan.finelli@hotmail.com>
2017-01-03 12:27:35 -0800

    fixed sign-up and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user_id
+      session[:user_id] = @user.id
       redirect_to '/profile'
     else
       redirect_to '/signup'
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(User.last)
+    @user = User.find(session[:user_id])
+
   end
 
   def edit
