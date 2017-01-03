72126aab403ebebce252953c88836d1e87b129d2
margarh1 <ha.margaret1@gmail.com>
2017-01-03 12:27:42 -0800

    troubleshot signup and login pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user_id
+      session[:user_id] = @user.id
       redirect_to '/profile'
     else
       redirect_to '/signup'
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(User.last)
+    @user = User.find(session[:user_id])
   end
 
   def edit
@@ -26,10 +26,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-  def user_id
-    params[:id]
-  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
