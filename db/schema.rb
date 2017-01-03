d2cee36d4322ee1fcf61c505ec777e5c4a1885a6
Derry Nixon <nixondc93@gmail.com>
2017-01-03 13:53:17 -0800

    added city column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103183942) do
+ActiveRecord::Schema.define(version: 20170103215017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170103183942) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "city"
   end
 
 end
