0489615cc91b56d3af9e7f6b4d2a90b938840152
Derry Nixon <nixondc93@gmail.com>
2017-01-03 15:05:09 -0800

    added user and post association

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103215017) do
+ActiveRecord::Schema.define(version: 20170103230232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 20170103215017) do
     t.string   "city"
   end
 
+  add_foreign_key "posts", "users"
 end
