7a3f7b9c9cd698d0ad19ce634e2fd366fddba18c
margarh1 <ha.margaret1@gmail.com>
2017-01-03 11:06:34 -0800

    created @ objects for erb stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,22 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by({username: user_params[:username]})
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to profile_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,32 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
   end
 
   def show
+    @user = User.find(user_id)
   end
 
   def edit
+    @user = User.find(user_id)
   end
 
   def update
+  end
+
+  private
+
+  def user_id
+    params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 
 end
