8c17ad41c455e314f68a4f985dc4142389421f4d
Kevin Tran <kevin2005tran@gmail.com>
2017-01-03 17:41:25 -0800

    finished new post page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   get '/users/:id' => 'users#show'
-  get '/posts/:id' => 'posts#show', as: 'post'
   get '/profile' => 'users#profile'
   get '/edit' => 'users#edit'
   put '/profile' => 'users#update'
@@ -17,5 +16,9 @@ Rails.application.routes.draw do
 
   get '/cities' => 'cities#index'
   get '/cities/:name' => 'cities#show', as: 'city'
+
+  get '/posts/new' => 'posts#new', as: 'newpost'
+  post '/posts' => 'posts#create', as: 'createpost'
+  get '/posts/:id' => 'posts#show', as: 'post'
 
 end
